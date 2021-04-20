a79c074480efef219e3f24c9a0c6ec2f0b092e42
wangfeng <0049003159@znv.com>
2021-04-20 17:31:49 +0800

    ,,

---
+++ mysql/源码分析/组复制/Delegate.cc
@@ -0,0 +1,6 @@
+Delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	100
+	Binlog_relay_IO_delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	370
+	Binlog_storage_delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	325
+	Binlog_transmit_delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	344
+	Server_state_delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	301
+	Trans_delegate		rpl_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\sql)	275
---
+++ mysql/源码分析/组复制/Group_event_observer.cc
@@ -0,0 +1,8 @@
+Group_event_observer		group_event_observer.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\plugin_observers)	39
+	Group_action_coordinator		group_action_coordinator.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\group_actions)	53
+	Multi_primary_migration_action		multi_primary_migration_action.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\group_actions)	35
+	Primary_election_action		primary_election_action.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\group_actions)	35
+	Primary_election_primary_process		primary_election_primary_process.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\plugin_handlers)	40
+	Primary_election_secondary_process		primary_election_secondary_process.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\plugin_handlers)	39
+	Primary_election_validation_handler		primary_election_validation_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\plugin_handlers)	31
+	Remote_clone_handler		remote_clone_handler.h (D:\wangfeng\Virtual_Machines\share\mysql-boost-8.0.23\mysql-8.0.23\plugin\group_replication\include\plugin_handlers)	34
