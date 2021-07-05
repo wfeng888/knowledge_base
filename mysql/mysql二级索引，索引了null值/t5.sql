5ee3a7dd50bcf87d02c68b7cbf255ff08c15c466
wangfeng <0049003159@znv.com>
2021-07-05 19:53:25 +0800

    ,,

--- mysql/mysql二级索引，索引了null值/t5.sql
+++ mysql/mysql二级索引，索引了null值/t5.sql
@@ -5,23 +5,28 @@ CREATE TABLE `t5` (
   KEY `c1` (`c1`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(1,1);
-Query OK, 1 row affected (0.00 sec)
+insert into t5 values(1,1);
+insert into t5 values(2,null);
+insert into t5 values(3,3);
+insert into t5 values(4,null);
+insert into t5 values(5,5);
+insert into t5 values(6,null);
+flush tables;
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(2,null);
-Query OK, 1 row affected (0.00 sec)
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(3,3);
-Query OK, 1 row affected (0.00 sec)
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(4,null);
-Query OK, 1 row affected (0.00 sec)
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(5,5);
-Query OK, 1 row affected (0.00 sec)
+CREATE TABLE `t8` (
+  `id` int NOT NULL,
+  `c1` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `c1` (`c1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-(root@localhost:3300.socket) [wftest]> insert into t5 values(6,null);
-Query OK, 1 row affected (0.00 sec)
-
-(root@localhost:3300.socket) [wftest]> flush tables;
-Query OK, 0 rows affected (0.01 sec)
+insert into t8 values(1,1);
+insert into t8 values(2,2);
+insert into t8 values(3,3);
+insert into t8 values(4,4);
+insert into t8 values(5,5);
+insert into t8 values(6,6);
+flush tables;
